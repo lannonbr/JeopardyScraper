4fece3def3eeda911dd8daf02352490a546051e3
Benjamin Lannon <benjamin@lannonbr.com>
2019-05-30 21:53:09 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jeopardy Scraper
+
+James Holzhauer has been on a Streak of continually winning Jeopardy. There is a site called [TheJeopardyFan.com](https://thejeopardyfan.com/) that blogs results of Jeopardy games.
+
+I wrote a simple script using Puppeteer to extract data from the site including the current day's winnings, current streak, and total winnings for James.
+
+There are plenty of kinks in the scraper currently such as if James does not play a day for some event on Jeopardy or if the blog post hasn't been published that day. This will be improved over time.
---
+++ index.js
@@ -0,0 +1,45 @@
+const puppeteer = require("puppeteer");
+const moment = require("moment");
+const matchAll = require("string.prototype.matchall");
+
+(async () => {
+  let time = moment();
+  let year = time.format("YYYY");
+  let month = time.format("MM");
+  let date = time.format("M-D-YYYY");
+
+  // Skip on weekends
+  if (time.day() === 0 || time.day() === 6) return;
+
+  // URL for the blog post for today
+  let url = `https://thejeopardyfan.com/${year}/${month}/final-jeopardy-${date}.html`;
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.waitFor(1000);
+
+  const content = await page.evaluate(() => {
+    let content = [...document.querySelectorAll(".entry-content span")]
+      .map(span => span.innerText)
+      .filter(text => text.includes("James"))[0];
+    return content;
+  });
+
+  // Stop if it doesn't find any content
+  if (content === undefined) return;
+
+  let [today, total] = [...matchAll(content, /(\$[\d,]*)/gm)]
+    .slice(2)
+    .map(match => match[0]);
+
+  let streak = +[...matchAll(content, /(\d*-day)/gm)][0][0].slice(0, -4);
+
+  let todayStr = time.format("ll");
+
+  console.log(
+    `Today, ${todayStr}, James Holzhauer has made ${today} to a ${streak} day total of ${total}`
+  );
+
+  browser.close();
+})();
